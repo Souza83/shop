572ad956fb09f7c5da2a6f18a6d28d3c0ec55974
Souza83 <tec.psouza@gmail.com>
2022-05-12 04:28:53 -0300

    Extraindo Grid de Produtos.

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/product_item.dart';
+
+import '../models/product.dart';
+import '../models/product_list.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        // P/ scrooll
+        crossAxisCount: 2, // Qtd de produtos por linha
+        childAspectRatio: 3 / 2, //Proporção da dimenção altura/largura
+        crossAxisSpacing: 10, // Espaçamento no eixo cruzado
+        mainAxisSpacing: 10, // Espaçamento no eixo principal
+      ),
+    );
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
-import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          // P/ scrooll
-          crossAxisCount: 2, // Qtd de produtos por linha
-          childAspectRatio: 3 / 2, //Proporção da dimenção altura/largura
-          crossAxisSpacing: 10, // Espaçamento no eixo cruzado
-          mainAxisSpacing: 10, // Espaçamento no eixo principal
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
