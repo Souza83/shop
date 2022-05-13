a80517f66a083abbd7dd5ea28df8c71e3222d429
Souza83 <tec.psouza@gmail.com>
2022-05-13 05:13:33 -0300

    Filtrar Favoritos de Forma Global #01.

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,9 +5,26 @@ import 'package:shop/models/product.dart';
 // ChangeNotifier: auxilia na reatividade. With: mixin da classe (add classe)
 class ProductList with ChangeNotifier {
   List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
   // [..._items]: recebe um clone da lista deixando mais seguro
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    // where: método filter (filtra os dados da aplicação)
+    if (_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
