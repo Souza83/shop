0b4dd9324943b09f99c1a5dbb6bc4f3b4eeb2915
Souza83 <tec.psouza@gmail.com>
2022-05-16 05:28:35 -0300

    Carrinho e Item do Carrinho.

---
+++ lib/models/cart.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/product.dart';
+
+// Mixin ChangeNotifier: para gerenciar o estado da aplicação
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {...items}; // {...items}: gera clone em caso de acesso indevido
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  // Quantidade total do carrinho
+  double get totalAmount {
+    double total = 0.0;
+    items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (existingItem) => CartItem(
+          id: existingItem.id,
+          productId: existingItem.productId,
+          name: existingItem.name,
+          quantity: existingItem.quantity + 1,
+          price: existingItem.price,
+        ),
+      );
+    } else {
+      // putIfAbsent: insira se não estiver presente.
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          productId: product.id,
+          name: product.name,
+          quantity: 1,
+          price: product.price,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners(); // Faz notificação de estado
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
+}
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // with ChangeNotifier: mixin com classe de programção reativa
 class Product with ChangeNotifier {
   final String id;
-  final String title;
+  final String name;
   final String description;
   final double price;
   final String imageUrl;
@@ -12,7 +12,7 @@ class Product with ChangeNotifier {
   //Construtor Product
   Product({
     required this.id,
-    required this.title,
+    required this.name,
     required this.description,
     required this.price,
     required this.imageUrl,
