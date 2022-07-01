75b264200e057cbf3648d4eaf279c9c6fba7aca1
Souza83 <tec.psouza@gmail.com>
2022-07-01 14:35:57 -0300

    Login de Usuário.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -7,9 +7,13 @@ class Auth with ChangeNotifier {
   // Consultar firebase.google.com/docs | Firebase Auth REST API | Sign up with email / password
   static const _url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDa1RTiZe7QpKPt_y8KT2H9mS_O2h6-eIE';
-  Future<void> signup(String email, String password) async {
+
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyDa1RTiZe7QpKPt_y8KT2H9mS_O2h6-eIE';
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: jsonEncode({
         'email': email,
         'password': password,
@@ -18,5 +22,13 @@ class Auth with ChangeNotifier {
     );
 
     print(jsonDecode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, 'signInWithPassword');
   }
 }
