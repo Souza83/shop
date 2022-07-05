4dfdff14eeec092cb23f79382802e97c77defc7d
Souza83 <tec.psouza@gmail.com>
2022-07-05 05:10:49 -0300

    Gerenciando Token.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -5,8 +5,28 @@ import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
   // Consultar firebase.google.com/docs | Firebase Auth REST API | Sign up with email / password
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDa1RTiZe7QpKPt_y8KT2H9mS_O2h6-eIE';
+  String? _token;
+  String? _email;
+  String? _uid;
+  DateTime? _expiryDate;
+
+  // Método verifica autenticação (Se a data de expiração é no futuro)
+  bool get isAuth {
+    final isValid = _expiryDate?.isAfter(DateTime.now()) ?? false;
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+
+  String? get email {
+    return isAuth ? _email : null;
+  }
+
+  String? get uid {
+    return isAuth ? _uid : null;
+  }
 
   Future<void> _authenticate(
       String email, String password, String urlFragment) async {
@@ -24,9 +44,18 @@ class Auth with ChangeNotifier {
 
     if (body['error'] != null) {
       throw AuthException(body['error']['message']);
-    }
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _uid = body['localId'];
 
-    print(body);
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(body['expiresIn']),
+        ),
+      );
+      notifyListeners();
+    }
   }
 
   Future<void> signup(String email, String password) async {
